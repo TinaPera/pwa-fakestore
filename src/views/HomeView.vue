ae00fb32b2906130ae11292e185bca20b584f9a1
TinaPera <valentina.pera2007@gmail.com>
2024-03-12 14:42:46 -0300

    Criação de 5 novos menus

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,11 @@
 <script setup>
+ import ListagemProdutos from '@/components/ListagemProdutos.vue';
 </script>
 
 <template>
+    <ListagemProdutos />
   <main>
     <img src="/public/logo.jpg" alt="">
   </main>
 </template>
+
